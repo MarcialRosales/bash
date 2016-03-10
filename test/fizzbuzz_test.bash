1fd885f4ddc9fc1badd507debe127085f8f42e58
Gerhard Lazu <gerhard@lazu.co.uk>
2016-03-10 19:13:32 +0000

    Make first pending test fail
    Notice the hard-coded 3 in the test description. I am fine with it
    because I do not expect this test to ever change, only deleted and
    replaced with a more accurate/relevant one.
    
    I take immutability to the extreme: immutable infrastructure, immutable
    code, immutable tests. I strongly believe that composing simple
    concepts that do not change makes for better codebases.

--- test/fizzbuzz_test.bash
+++ test/fizzbuzz_test.bash
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+# basht macro, shellcheck fix
+export T_fail
+
 # shellcheck disable=SC1091
 . test/test_helpers
 
+# shellcheck disable=SC1091
+. lib/fizzbuzz.bash
+
 T_fizz_TrueWhen3() {
-  pending
+  fizz 3 ||
+  $T_fail "Expected to be true when 3"
 }
