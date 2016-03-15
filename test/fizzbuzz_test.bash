5cefabb7396c0e68de6a3b6c11101d5d2c7ee5c9
Gerhard Lazu <gerhard@lazu.co.uk>
2016-03-15 08:48:39 +0000

    Convert plain numbers into a range
    Bash is a powerful and expressive language. Expansion and redirection
    are by far my favourite features. If you want to learn more about bash
    brace expansion:
    
    http://wiki.bash-hackers.org/syntax/expansion/brace

--- test/fizzbuzz_test.bash
+++ test/fizzbuzz_test.bash
@@ -11,7 +11,7 @@ export T_fail
 
 T_fizzbuzz_GivenASeriesOfNumbersAsParameters() {
   local actual expected
-  actual="$(fizzbuzz 3 4 5 6 7 8 9 10 11 12 13 14 15)"
+  actual="$(fizzbuzz {3..15})"
   expected="Fizz
 4
 Buzz
